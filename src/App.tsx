3f98d55e1a9945f22c25fdedd971476637c24ed6
JunHee <jhlee@infracube.co.kr>
2022-11-27 22:52:20 +0900

    fixed readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Container, Button } from "@mui/material";
+import { WORDLIST } from "./mnemonic";
 
-function App() {
+const leng = 12;
+
+const App = () => {
+  const [key, setKey] = useState("");
+  const getRandom = (): number => {
+    return Number((Math.random() * 2047).toFixed(0));
+  };
+
+  const getWords = () => {
+    const target: string[] = [];
+    for (let i = 0; i < leng; i++) {
+      const txt = WORDLIST[getRandom()];
+      if (!target.includes(txt)) target.push(txt);
+      else i--;
+    }
+    return target;
+  };
+
+  const copyTxt = () => {
+    let txt = "";
+    getWords().forEach((w) => {
+      txt += `${w} `;
+    });
+    window.navigator.clipboard.writeText(txt);
+    console.log(txt);
+
+    setKey(txt);
+  };
+
+  const reCopy = () => {
+    window.navigator.clipboard.writeText(key);
+  };
+
+  // useEffect(() => {
+  //   window.addEventListener("focus", () => {
+  //     copyTxt();
+  //   });
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container sx={{ width: "100vw", height: "100vh" }}>
+      <h1>Mnemonic Generator</h1>
+      <Button
+        variant="contained"
+        sx={{ height: "45px", marginRight: "1rem" }}
+        onClick={copyTxt}
+      >
+        Generate!
+      </Button>
+      <Button variant="contained" sx={{ height: "45px" }} onClick={reCopy}>
+        ReCopy!
+      </Button>
+    </Container>
   );
-}
+};
 
 export default App;
